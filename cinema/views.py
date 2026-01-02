70686c3b42a84cfc5bb7535c120ee603282174f0
Anastasiia Duzhynska <a.duzhynska@gmail.com>
2026-01-02 16:12:28 +0200

    Fix flake8 again

--- cinema/views.py
+++ cinema/views.py
@@ -67,9 +67,9 @@ class MovieSessionViewSet(viewsets.ModelViewSet):
     def get_queryset(self) -> QuerySet[MovieSession]:
         queryset = self.queryset
         if self.action in ("list", "retrieve"):
-            queryset = (MovieSession.objects.
-                        select_related(
-                "movie",
-                "cinema_hall")
+            queryset = (
+                MovieSession.objects.select_related("movie",
+                                                    "cinema_hall"
+                                                    )
             )
         return queryset
